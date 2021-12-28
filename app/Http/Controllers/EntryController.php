aaaaba5807aca894cf4910d3d6e5b2d387ddef80
hunter20200215 <spravko.freelancer@gmail.com>
2021-12-28 11:09:19 +0100

    making sort by date created finally

--- app/Http/Controllers/EntryController.php
+++ app/Http/Controllers/EntryController.php
@@ -67,6 +67,7 @@ class EntryController extends Controller
             'sortName' => 'desc',
             'sortPhone' => 'desc',
             'sortCreatedBy' =>'desc',
+            'sortDataAdded' => 'desc',
 
         ]);
         
@@ -622,6 +623,7 @@ class EntryController extends Controller
             'sortId' => 'desc',
             'sortName' => 'desc',
             'sortCreatedBy' =>'desc',
+            'sortDataAdded' => 'desc',
 
         ]);
         // return redirect()->route('admin.clients',['clients' =>$clients]);
@@ -709,6 +711,7 @@ class EntryController extends Controller
             'sortName' => 'desc',
             'counters' => $counters,
             'sortCreatedBy' =>'desc',
+            'sortDataAdded' => 'desc',
         ]);
     }
 
@@ -903,6 +906,7 @@ class EntryController extends Controller
             'sortName' => 'desc',
             'sortPhone' => 'desc',
             'sortCreatedBy' =>'desc',
+            'sortDataAdded' => 'desc',
         ]);
     }
 
@@ -1085,6 +1089,7 @@ class EntryController extends Controller
             'sortName' => $sortName,
             'sortPhone' => 'desc',
             'sortCreatedBy' =>'desc',
+            'sortDataAdded' => 'desc',
         ]);
     }
 
@@ -1267,6 +1272,7 @@ class EntryController extends Controller
             'sortName' => 'desc',
             'sortPhone' => $sortPhone,
             'sortCreatedBy' =>'desc',
+            'sortDataAdded' => 'desc',
         ]);
     }
 
@@ -1500,6 +1506,224 @@ class EntryController extends Controller
             'sortName' => 'desc',
             'sortPhone' => 'desc',
             'sortCreatedBy' =>$sortCreatedBy,
+            'sortDataAdded' => 'desc',
+        ]);
+    }
+
+    public function entrySortByDataAdded(Request $request)
+    {   
+        $categorys=[];
+        $income = [];
+        $deduction =[];
+        $rolls =[];
+        $sortDataAdded ="";
+        $full_name = $request->full_name1;
+        $sets = explode(",", $request->sets);
+        $start_date = $request->start_date;
+        $end_date = $request->end_date;
+        if ($request->sortDataAdded == "desc") {
+            $sortDataAdded = 'asc';
+        } else {
+            $sortDataAdded = 'desc';
+        }
+
+        if ($request->categorys || $request->income || $request->deduction || $request->start_date1 || $request->end_date1) {
+            if ($request->categorys) {
+                $categorys = explode(",", $request->categorys);
+            } else {
+                $categorys = [];
+            }
+
+            if ($request->income) {
+                $income = explode(",", $request->income);
+            } else {
+                $income = [];
+            }
+
+            if ($request->deduction) {
+                $deduction = explode(",", $request->deduction);
+            } else {
+                $deduction = [];
+            }
+            
+            $clients = DB::table('admin_clients_info')
+                    ->leftJoin('users', function ($join) {
+                        $join->on('admin_clients_info.user_id', '=', 'users.id');
+                    })
+                    ->select('admin_clients_info.*','users.name as created_name')
+                    ->when($categorys, function ($query, $categorys) {
+                        return $query->whereIn('category', $categorys);
+                    })
+                    ->when($income, function ($query, $income) {
+                        return $query->where(function($query) use ($income) {
+                            foreach ($income as $value) {
+                                $query->where('income_highlights', 'LIKE', "%".",".$value.","."%");
+                            }
+                        });
+                    })
+                    ->when($deduction, function ($query, $deduction) {
+                        return $query->where(function($query) use ($deduction) {
+                                foreach ($deduction as $value) {
+                                    $query->where('deduction_highlights', 'LIKE', "%".",".$value.",".'%');
+                                }
+                            });
+                    })
+                    ->when( $start_date, function ($query, $start_date) {
+                        return $query->where('dob_date', ">=", $start_date);
+                    })
+                    ->when($end_date, function ($query, $end_date) {
+                        return $query->where('dob_date', "<",$end_date);
+                    })
+                    ->orderBy('created_at', $sortDataAdded)
+                    ->paginate(100);
+            $counters = DB::table('admin_clients_info')
+                    ->leftJoin('users', function ($join) {
+                        $join->on('admin_clients_info.user_id', '=', 'users.id');
+                    })
+                    ->select('admin_clients_info.*','users.name as created_name')
+                    ->when($categorys, function ($query, $categorys) {
+                        return $query->whereIn('category', $categorys);
+                    })
+                    ->when($income, function ($query, $income) {
+                        return $query->where(function($query) use ($income) {
+                            foreach ($income as $value) {
+                                $query->where('income_highlights', 'LIKE', "%".",".$value.","."%");
+                            }
+                        });
+                    })
+                    ->when($deduction, function ($query, $deduction) {
+                        return $query->where(function($query) use ($deduction) {
+                                foreach ($deduction as $value) {
+                                    $query->where('deduction_highlights', 'LIKE', "%".",".$value.",".'%');
+                                }
+                            });
+                    })
+                    ->when( $start_date, function ($query, $start_date) {
+                        return $query->where('dob_date', ">=", $start_date);
+                    })
+                    ->when($end_date, function ($query, $end_date) {
+                        return $query->where('dob_date', "<",$end_date);
+                    })
+                    ->count();
+            
+            foreach ($clients as $client) {
+                $roll = DB::table('users')
+                        ->where('id' ,'=',$client->user_id)
+                        ->pluck('name');
+                array_push($rolls,$roll[0]);
+            }
+        } elseif ($full_name) {
+            if (count($sets) == 2) {
+                $clients = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('full_name', 'LIKE', "%".$full_name.'%')
+                        ->orWhere('client_bio', 'LIKE', "%".$full_name.'%')
+                        ->orderBy('created_at', $sortDataAdded)
+                        ->paginate(100);
+                $counters = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('full_name', 'LIKE', "%".$full_name.'%')
+                        ->orWhere('client_bio', 'LIKE', "%".$full_name.'%')
+                        ->count();
+            } elseif ($sets[0]==0) {
+                $clients = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('full_name', 'LIKE', "%".$full_name.'%')
+                        ->orderBy('created_at', $sortDataAdded)
+                        ->paginate(100);
+                $counters = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('full_name', 'LIKE', "%".$full_name.'%')
+                        ->count();
+            } elseif ($sets[0] == 1) {
+                $clients = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('client_bio', 'LIKE', "%".$full_name.'%')
+                        ->orderBy('created_at', $sortDataAdded)
+                        ->paginate(100);
+                $counters = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('client_bio', 'LIKE', "%".$full_name.'%')
+                        ->count();
+            } else {
+                $clients = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('full_name', 'LIKE', "%".$full_name.'%')
+                        ->orderBy('created_at', $sortDataAdded)
+                        ->paginate(100);
+                $counters = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->where('full_name', 'LIKE', "%".$full_name.'%')
+                        ->count();
+            }
+
+            foreach ($clients as $client) {
+                $roll = DB::table('users')
+                        ->where('id' ,'=',$client->user_id)
+                        ->pluck('name');
+                array_push($rolls,$roll[0]);
+            }
+        
+        } else {
+            $clients = DB::table('admin_clients_info')
+                        ->leftJoin('users', function ($join) {
+                            $join->on('admin_clients_info.user_id', '=', 'users.id');
+                        })
+                        ->select('admin_clients_info.*','users.name as created_name')
+                        ->orderBy('created_at', $sortDataAdded)
+                        ->paginate(100);
+            $counters = DB::table('admin_clients_info')
+                        ->count();
+            foreach ($clients as $client) {
+                $roll = DB::table('users')
+                        ->where('id' ,'=',$client->user_id)
+                        ->pluck('name');
+                array_push($rolls,$roll[0]);
+            }
+
+        }
+        return view('entryClients',[
+            'clients' =>$clients,
+            'categorys' => DB::table('admin_category')->get(),
+            'highlights' => DB::table('admin_highlights')->get(),
+            'incomehighlights' => DB::table('admin_income_highlights')->get(),
+            'deductionhighlights' => DB::table('admin_deduction_highlights')->get(),
+            'selected_categorys' => $categorys,
+            'selected_income' => $income,
+            'selected_deduction' =>$deduction,
+            'rolls' =>$rolls,
+            'sets' => $sets,
+            'full_name' => $full_name,
+            'counters' => $counters,
+            'sortId' => 'desc',
+            'sortName' => 'desc',
+            'sortPhone' => 'desc',
+            'sortCreatedBy' =>'desc',
+            'sortDataAdded' =>$sortDataAdded,
         ]);
     }
     
--- routes/web.php
+++ routes/web.php
@@ -114,6 +114,7 @@ Route::get('entry/sort-by-ID', [App\Http\Controllers\EntryController::class, 'en
 Route::get('entry/sort-by-Name', [App\Http\Controllers\EntryController::class, 'entrySortByName'])->name('entry.clients.sortname');
 Route::get('entry/sort-by-Phone', [App\Http\Controllers\EntryController::class, 'entrySortByPhone'])->name('entry.clients.sortphone');
 Route::get('entry/sort-by-CreateBy', [App\Http\Controllers\EntryController::class, 'entrySortByCreatedBy'])->name('entry.clients.createdby');
+Route::get('entry/sort-by-Data-Added', [App\Http\Controllers\EntryController::class, 'entrySortByDataAdded'])->name('entry.clients.dataadded');
 
 #Book-keeper
 Route::get('/bookkeepr/home', [App\Http\Controllers\BookkeeperController::class, 'index'])->name('bookkeeper.home')->middleware('is_bookkeeper');
